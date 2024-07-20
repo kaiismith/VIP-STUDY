695322317076c04e37649f8c731d38ce1d60968e
kaiismith <thangdd.bi12-406@st.usth.edu.vn>
2024-07-20 18:23:19 +0700

    Modify ENV

--- main.go
+++ main.go
@@ -18,11 +18,11 @@ type Student struct {
 
 func main() {
 	// Read database connection details from environment variables set by Railway
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
+	host := os.Getenv("PGHOST")
+	port := os.Getenv("PGPORT")
+	user := os.Getenv("PGUSER")
+	password := os.Getenv("PGPASSWORD")
+	dbname := os.Getenv("PGNAME")
 
 	// Construct the connection string
 	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
